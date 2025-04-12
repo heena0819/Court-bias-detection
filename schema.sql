4196cb8b403883174d167daac282e9cc5487a455
Heena <missheena0819@gmail.com>
2025-04-12 15:07:02 +0530

    Add files via upload

--- schema.sql
+++ schema.sql
@@ -1,26 +1,25 @@
-CREATE TABLE influencers(
-	id SERIAL PRIMARY KEY,
-	name VARCHAR(100),
-	platform VARCHAR(50),
-	followers INT,
-	engagement_rate DECIMAL(4, 2),
-	niche VARCHAR(50)
+CREATE TABLE defendants (
+    defendant_id INT PRIMARY KEY,
+    name TEXT,
+    race TEXT,
+    gender TEXT,
+    age INT
 );
 
-CREATE TABLE bios(
-	influencer_id INT REFERENCES influencers(id),
-	bio_text TEXT,
-	link_in_bio VARCHAR(255)
+CREATE TABLE judges (
+    judge_id INT PRIMARY KEY,
+    name TEXT,
+    court_location TEXT
 );
 
-CREATE TABLE brand_links(
-	brand_id SERIAL PRIMARY KEY,
-	brand_name VARCHAR(255),
-	domain VARCHAR(100)
-);
-
-CREATE TABLE brand_collabs(
-	influencer_id INT REFERENCES influencers(id),
-	brand_id INT REFERENCES brand_links(brand_id),
-	match_type VARCHAR(20)
+CREATE TABLE cases (
+    case_id INT PRIMARY KEY,
+    defendant_id INT,
+    judge_id INT,
+    crime_type TEXT,
+    sentence_length INT,
+    verdict TEXT,
+    case_date DATE,
+    FOREIGN KEY (defendant_id) REFERENCES defendants(defendant_id),
+    FOREIGN KEY (judge_id) REFERENCES judges(judge_id)
 );
